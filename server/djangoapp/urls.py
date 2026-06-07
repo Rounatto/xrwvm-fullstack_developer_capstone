235920f296266fc209ad8d0bbcb44eb9fc918db1
Rounatto <rayan1mhalla@gmail.com>
2026-06-07 16:41:35 +0100

    Style tweaks and minor updates

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('register/', views.registration, name='register'),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('get_cars/', views.get_cars, name='get_cars'),
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
